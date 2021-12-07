9c24e5c33d603af7aff008f9cc80d1e0a55b4560
yuxiang0718 <yuxiang0718@gmail.com>
2021-12-07 11:43:08 +0800

    school number

--- dht.ino
+++ dht.ino
@@ -1,9 +1,9 @@
 #include <dht.h>     
-#define dht_dpin A0 	             
+#define dht_dpin A0 	           //DHT 11 類比輸入 Pin A0  
 dht DHT;   
 
 String a;
-String Sch_no = "555";
+String Sch_no = "249";
 
 void setup(){  
   Serial.begin(9600);   
